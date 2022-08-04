19b7a4a6623c41a7028ad0f29446c5e76e1bff33
Michal Gosek <inbox@michalgosek.com>
2022-08-04 21:05:04 +0200

    added greeter component

--- frontend/src/components/navigation/main-nav.tsx
+++ frontend/src/components/navigation/main-nav.tsx
@@ -1,28 +1,53 @@
+import { FC } from 'react';
 import { GetLinkClassName } from './utils';
 import { Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import ParticipantNav from './participant-nav';
 import { TRAININGS_SERVICE_ROLES } from 'authorization/role-authorization-hook';
 import TrainerNav from './trainer-nav';
+import { useAuth0 } from '@auth0/auth0-react';
 import { useGetAuthorization } from 'authorization';
 
-const MainNav: React.FC = () => {
+const Greeter: FC = () => {
+    const { user } = useAuth0();
     const isTrainer = useGetAuthorization([TRAININGS_SERVICE_ROLES.TRAINER]);
     const isParticipant = useGetAuthorization([TRAININGS_SERVICE_ROLES.PARTICIPANT]);
 
+    if (!user) return null;
+
+    const name = user.name;
+    const role = isTrainer ? TRAININGS_SERVICE_ROLES.TRAINER : isParticipant ? TRAININGS_SERVICE_ROLES.PARTICIPANT : "";
     return (
-        <Nav className='navbar-nav mr-auto'>
-            <NavLink to='/' className={({ isActive }) => GetLinkClassName(isActive)}>
-                Home
-            </NavLink>
-            <NavLink to='/profile' className={({ isActive }) => GetLinkClassName(isActive)}>
-                Profile
-            </NavLink>
-            <NavLink to='/trainings' className={({ isActive }) => GetLinkClassName(isActive)}>
-                Trainings
-            </NavLink>
-            {isTrainer ? <TrainerNav /> : isParticipant ? <ParticipantNav /> : null}
-        </Nav >
+        <div className='text-left'>
+            <p className='small'> Hello, <b>{name}</b> 👋 ! You're logged as <b>{role}</b>. Have fun! 💪</p>
+        </div>
+    );
+}
+
+const MainNav: FC = () => {
+    const isTrainer = useGetAuthorization([TRAININGS_SERVICE_ROLES.TRAINER]);
+    const isParticipant = useGetAuthorization([TRAININGS_SERVICE_ROLES.PARTICIPANT]);
+    return (
+        <>
+
+            <Nav className='navbar-nav mr-auto'>
+                <NavLink to='/' className={({ isActive }) => GetLinkClassName(isActive)}>
+                    Home
+                </NavLink>
+                <NavLink to='/profile' className={({ isActive }) => GetLinkClassName(isActive)}>
+                    Profile
+                </NavLink>
+                <NavLink to='/trainings' className={({ isActive }) => GetLinkClassName(isActive)}>
+                    Trainings
+                </NavLink>
+                {isTrainer ? <TrainerNav /> : isParticipant ? <ParticipantNav /> : null}
+            </Nav >
+            <Greeter />
+        </>
+
+
+
+
     );
 };
 
